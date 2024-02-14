c7ed685e6c1f71dcefa3948aef743e9d51e80449
devnem0y <devnem0y@gmail.com>
2024-02-14 15:40:37 +0500

    +

--- Assets/UHArchitecture/Kit/UISystem/WSettings.cs
+++ Assets/UHArchitecture/Kit/UISystem/WSettings.cs
@@ -6,18 +6,28 @@ using UralHedgehog.UI;
 public class WSettings : Widget
 {
     [SerializeField] private Button _btnClose;
+    [SerializeField] private Button _btnBackMain;
 
     private ISettings _settings;
 
     protected override void Awake()
     {
         base.Awake();
+        
         _btnClose.onClick.AddListener(Hide);
+        
+        _btnBackMain.onClick.AddListener(() =>
+        {
+            Hide();
+            Game.Instance.ChangeState(GameState.MAIN);
+        });
     }
 
     public override void Init(params object[] param)
     {
         _settings = (ISettings) param[0];
+        
+        _btnBackMain.gameObject.SetActive(Game.Instance.GameState == GameState.PLAY);
     }
 
     public override void Show()
--- Assets/UHProject/Screens/Main/PBattle.cs
+++ Assets/UHProject/Screens/Main/PBattle.cs
@@ -5,13 +5,5 @@ using UralHedgehog.UI;
 
 public class PBattle : Widget
 {
-    [SerializeField] private Button _btnBackMain;
-
-    public override void Init()
-    {
-        _btnBackMain.onClick.AddListener(() =>
-        {
-            Game.Instance.ChangeState(GameState.MAIN);
-        });
-    }
+    
 }
